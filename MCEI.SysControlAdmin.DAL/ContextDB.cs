53bddd4270f04ce325b2f92fbf560fc548fd1954
Hector Garcia <hg425823@gmail.com>
2025-03-16 21:39:31 -0600

    CREACION Y CONFIGURACION DE CLASE CONTEXTDB - CONECCION A LA BASE DE DATOS

---
+++ MCEI.SysControlAdmin.DAL/ContextDB.cs
@@ -0,0 +1,31 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using Microsoft.EntityFrameworkCore;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.DAL
+{
+    public class ContextDB : DbContext
+    {
+        #region REFERENCIAS DE TABLAS DE LA BD
+        //Coleccion que hace referencia a las tablas de la base de datos
+        
+        #endregion
+
+        #region STRING DE CONEXION
+        // Metodo de Conexion a la Base de Datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=MCEISysControlAdminDB;Integrated Security=True;Trust Server Certificate=True"); // String de Conexion
+        }
+        #endregion
+    }
+}
