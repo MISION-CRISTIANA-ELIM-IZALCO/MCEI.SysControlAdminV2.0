b14359617937b5ce3c4a28e2e5ddbb578848fe8b
Hector Garcia <hg425823@gmail.com>
2025-03-18 19:07:31 -0600

    LOGICA CONTROLLER Y VISTA PARA CREAR USUARIO

---
+++ MCEI.SysControlAdmin.WebApp/Controllers/User - Controller/UserController.cs
@@ -0,0 +1,63 @@
+﻿#region REFERENCIAS
+// Referencias Necesarios Para El Correcto Funcionamiento
+using MCEI.SysControlAdmin.BL.Role___BL;
+using MCEI.SysControlAdmin.BL.User___BL;
+using MCEI.SysControlAdmin.EN.User___EN;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.WebApp.Controllers.User___Controller
+{
+    public class UserController : Controller
+    {
+        // Creamos Las Instancias Para Acceder a Los Metodos
+        UserBL userBL = new UserBL();
+        RoleBL roleBL = new RoleBL();
+
+        #region METODO PARA GUARDAR
+        // Accion Que Muestra El Formulario
+        public async Task<IActionResult> Create()
+        {
+            var roles = await roleBL.GetAllAsync();
+            ViewBag.Roles = roles;
+            return View();
+        }
+
+        // Accion Que Recibe Los Datos y Los Envia a La Base De Datos
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(User user, IFormFile imagen)
+        {
+            try
+            {
+                // Mapeo de img a ArrayByte
+                if (imagen != null && imagen.Length > 0)
+                {
+                    byte[] imagenData = null!;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await imagen.CopyToAsync(memoryStream);
+                        imagenData = memoryStream.ToArray();
+                    }
+
+                    user.ImageData = imagenData; // Asigna el array de bytes al campo de la imagen en tu modelo Membership
+                }
+                user.DateCreated = DateTime.Now;
+                user.DateModification = DateTime.Now;
+                int result = await userBL.CreateAsync(user);
+                TempData["SuccessMessageCreate"] = "Usuario Agregado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ViewBag.Error = e.Message;
+                ViewBag.Roles = await roleBL.GetAllAsync();
+                return View(user);
+            }
+        }
+        #endregion
+    }
+}
