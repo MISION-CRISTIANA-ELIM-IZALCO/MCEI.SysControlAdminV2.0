02e46f59a5f512f8e47a4ee17f1a2558a22ef8b9
Hector Garcia <hg425823@gmail.com>
2025-03-22 00:18:51 -0600

    CREACION DE CONTROLLER Y METODO PARA GUARDAR MEMBERSHIP

---
+++ MCEI.SysControlAdmin.WebApp/Controllers/Membership - Controller/MembershipController.cs
@@ -0,0 +1,65 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysControlAdmin.BL.Membership___BL;
+using MCEI.SysControlAdmin.EN.Membership___EN;
+using MCEI.SysControlAdmin.EN.Role___EN;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.WebApp.Controllers.Membership___Controller
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Desarrollador, Administrador, Digitador")]
+    public class MembershipController : Controller
+    {
+        // Creamos las instancias para acceder a los metodos
+        MembershipBL membershipBL = new MembershipBL();
+
+        #region METODO PARA CREAR
+        // Accion Para Mostrar La Vista De Crear
+        [Authorize(Roles = "Desarrollador, Administrador, Digitador")]
+        public IActionResult CreateMembership()
+        {
+            ViewBag.Error = "";
+            return View();
+        }
+
+        // Metodo que recibe y envia a la base de datos
+        [Authorize(Roles = "Desarrollador, Administrador, Digitador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateMembership(Membership membership, IFormFile imagen)
+        {
+            try
+            {
+                // Mapeo de img a ArrayByte
+                if (imagen != null && imagen.Length > 0)
+                {
+                    byte[] imagenData = null!;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await imagen.CopyToAsync(memoryStream);
+                        imagenData = memoryStream.ToArray();
+                    }
+
+                    membership.ImageData = imagenData; // Asigna el array de bytes al campo de la imagen en tu modelo Membership
+                }
+                membership.DateCreated = DateTime.Now;
+                membership.DateModification = DateTime.Now;
+                int result = await membershipBL.CreateAsync(membership);
+                TempData["SuccessMessageCreate"] = "Membresia Agregada Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(membership);
+            }
+        }
+        #endregion
+    }
+}
