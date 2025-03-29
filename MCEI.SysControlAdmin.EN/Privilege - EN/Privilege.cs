a79a6ba6745cca33a019fb70d2e11cab07831731
Hector Garcia <hg425823@gmail.com>
2025-03-29 10:46:46 -0600

    CREACION DE ENTIDAD PRIVILEGE

---
+++ MCEI.SysControlAdmin.EN/Privilege - EN/Privilege.cs
@@ -0,0 +1,53 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.EN.Privilege___EN
+{
+    public class Privilege
+    {
+        #region ATRIBUTOS DE LA ENTIDAD
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        [Display(Name = "Nombre")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ/ ]+$", ErrorMessage = "El Nombre debe contener solo letras")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Estado Es Requerido")]
+        [Display(Name = "Estado")]
+        public byte Status { get; set; }
+
+        [Display(Name = "Fecha De Creación")]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Fecha De Modificación")]
+        public DateTime DateModification { get; set; }
+        #endregion
+
+        #region ATRIBUTOS NO MAPEABLES
+        // Propiedad para formatear la fecha automáticamente
+        [NotMapped]
+        public string DateCreatedFormatted => DateCreated.ToString(@"dd/MM/yyyy");
+        [NotMapped]
+        public string DateModificationFormatted => DateModification.ToString(@"dd/MM/yyyy");
+
+        // Propiedad para formatear la hora con AM/PM
+        [NotMapped]
+        public string TimeCreatedFormatted => DateCreated.ToString("hh:mm tt");
+        [NotMapped]
+        public string TimeModificationFormatted => DateModification.ToString("hh:mm tt");
+        #endregion
+    }
+}
