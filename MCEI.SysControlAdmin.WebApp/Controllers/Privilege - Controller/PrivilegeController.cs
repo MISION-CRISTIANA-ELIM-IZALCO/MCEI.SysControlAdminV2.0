c5d7c71e2f3d3616e370feded7d855fee6e687d1
Hector Garcia <hg425823@gmail.com>
2025-03-29 13:22:57 -0600

    LOGICA CONTROLLER Y VISTAS PARA MODIFICAR PRIVILEGE

--- MCEI.SysControlAdmin.WebApp/Controllers/Privilege - Controller/PrivilegeController.cs
+++ MCEI.SysControlAdmin.WebApp/Controllers/Privilege - Controller/PrivilegeController.cs
@@ -2,6 +2,7 @@
 // Referencias Necesarias Para El Correcto Funcionamiento
 using MCEI.SysControlAdmin.BL.Privilege___BL;
 using MCEI.SysControlAdmin.EN.Privilege___EN;
+using MCEI.SysControlAdmin.EN.Role___EN;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -59,6 +60,37 @@ namespace MCEI.SysControlAdmin.WebApp.Controllers.Privilege___Controller
 
             var privileges = await privilegeBL.SearchAsync(privilege);
             return View(privileges);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo para mostar la vista de modificar
+        [Authorize(Roles = "Desarrollador, Administrador, Digitador")]
+        public async Task<IActionResult> EditPrivilege(int id)
+        {
+            var privilege = await privilegeBL.GetByIdAsync(new Privilege { Id = id });
+            ViewBag.Error = "";
+            return View(privilege);
+        }
+
+        // Metodo Que Recibe y Envia a La Base De Datos
+        [Authorize(Roles = "Desarrollador, Administrador, Digitador")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPrivilege(Privilege privilege)
+        {
+            try
+            {
+                privilege.DateModification = DateTime.Now;
+                int result = await privilegeBL.UpdateAsync(privilege);
+                TempData["SuccessMessageUpdate"] = "Privilegio Modificado Exitosamente";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+                return View(privilege);
+            }
         }
         #endregion
     }
