7198c56e07612d0c603aeab1906fe7ab2efda4b6
Hector Garcia <hg425823@gmaillcom>
2025-04-17 22:33:36 -0600

    CLASE BL Y SU LOGICA COMPLETA PARA BAPTISMS

---
+++ MCEI.SysControlAdmin.BL/Baptisms - BL/BaptismsBL.cs
@@ -0,0 +1,66 @@
+﻿#region REFERENCIAS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysControlAdmin.EN.Baptisms___EN;
+using MCEI.SysControlAdmin.DAL.Baptisms___DAL;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.BL.Baptisms___BL
+{
+    public class BaptismsBL
+    {
+        #region METODO PARA CREAR
+        // Metodo para guardar un nuevo registro
+        public async Task<int> CreateAsync(Baptisms baptisms)
+        {
+            return await BaptismsDAL.CreateAsync(baptisms);
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR
+        // Metodo para mostrar una lista de registros
+        public async Task<List<Baptisms>> GetAllAsync()
+        {
+            return await BaptismsDAL.GetAllAsync();
+        }
+        #endregion
+
+        #region METODO PARA MOSTRAR POR ID
+        // Metodo para mostrar un registro especifico bajo un id
+        public async Task<Baptisms> GetByIdAsync(Baptisms baptisms)
+        {
+            return await BaptismsDAL.GetByIdAsync(baptisms);
+        }
+        #endregion
+
+        #region METODO PARA BUSCAR
+        // Metodo para buscar registros existentes
+        public async Task<List<Baptisms>> SearchAsync(Baptisms baptisms)
+        {
+            return await BaptismsDAL.SearchAsync(baptisms);
+        }
+        #endregion
+
+        #region METODO PARA MODIFICAR
+        // Metodo para modificar un registro existente
+        public async Task<int> UpdateAsync(Baptisms baptisms)
+        {
+            return await BaptismsDAL.UpdateAsync(baptisms);
+        }
+        #endregion
+
+        #region METODO PARA ELIMINAR
+        // Metodo para eliminar un registro existente
+        public async Task<int> DeleteAsync(Baptisms baptisms)
+        {
+            return await BaptismsDAL.DeleteAsync(baptisms);
+        }
+        #endregion
+    }
+}
