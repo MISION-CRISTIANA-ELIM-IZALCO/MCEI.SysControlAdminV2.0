12c7d2776f072eafcf0a5c9105ad48f631572c13
Hector Garcia <hg425823@gmaillcom>
2025-04-18 11:37:16 -0600

    CREACION DE CONTROLLER PARA CARTA DE RECOMENDACION

---
+++ MCEI.SysControlAdmin.WebApp/Controllers/Recommendation - Controller/RecommendationController.cs
@@ -0,0 +1,49 @@
+﻿#region REFERENCIAS
+// Referencias Necesarias Para El Correcto Funcionamiento
+using MCEI.SysControlAdmin.BL.Membership___BL;
+using MCEI.SysControlAdmin.EN.Membership___EN;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+
+#endregion
+
+namespace MCEI.SysControlAdmin.WebApp.Controllers.Recommendation___Controller
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Desarrollador, Administrador")]
+    public class RecommendationController : Controller
+    {
+        // Creamos Una Instancia Para Acceder a Los Metodos
+        MembershipBL membershipBL = new MembershipBL();
+
+        #region METODO PARA MOSTRAR INDEX
+        // Accion Para Mostrar La Vista Index
+        [Authorize(Roles = "Desarrollador, Administrador")]
+        public async Task<IActionResult> Index(Membership membership = null!)
+        {
+            if (membership == null)
+                membership = new Membership();
+
+            var memberships = await membershipBL.SearchAsync(membership);
+            return View(memberships);
+        }
+        #endregion
+
+        #region METODO PARA REPORTE
+        // Metodo Para Generar Ficha o Reporte En PDF 
+        [Authorize(Roles = "Desarrollador, Administrador")]
+        public async Task<ActionResult> GeneratePDFfileRecomendation(int id)
+        {
+            var generatePDF = await membershipBL.GetByIdAsync(new Membership { Id = id });
+            string fileName = $"CartaDeRecomendacion_{generatePDF.Name}_{generatePDF.LastName}_{generatePDF.Dui}_MCEI.pdf";
+            generatePDF.DateNow = DateTime.Now.ToString("dd 'de' MMMM 'de' yyyy", new System.Globalization.CultureInfo("es-ES"));
+            return new ViewAsPdf("GeneratePDFfileRecomendation", generatePDF)
+            {
+                FileName = fileName
+            };
+        }
+        #endregion
+    }
+}
